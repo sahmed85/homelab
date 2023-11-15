609847f8426104f59f6bf468bcf3c4d5006d8d5f
Shadman <sahmed85@gatech.edu>
2023-11-14 19:08:31 -0500

    reprovisioning vms and amount of CPU and memory

--- homelab_infra/main.tf
+++ homelab_infra/main.tf
@@ -19,7 +19,7 @@ resource "proxmox_vm_qemu" "compute_server" {
   vmid = 100
   name        = "BreathofThunder"
   target_node = "homelab"
-  iso         = "local:iso/ubuntu-server-22.04.3.iso"
+  clone         = "local:iso/ubuntu-server-22.04.3.iso"
 
   # Specifying the VM parameters
   os_type = "ubuntu"  # Ubuntu Server
@@ -44,112 +44,78 @@ resource "proxmox_vm_qemu" "compute_server" {
   }
 }
 
-# control plane and etcd server
-resource "proxmox_vm_qemu" "vm_1" {
-  vmid = 201
-  name        = "homelab-k8s-cluster-vm1"
-  target_node = "homelab"
-  iso         = "local:iso/ubuntu-server-22.04.3.iso"
+# # control plane and etcd server
+# resource "proxmox_vm_qemu" "vm_1" {
+#   vmid = 201
+#   name        = "homelab-k8s-cluster-vm1"
+#   target_node = "homelab"
+#   iso         = "local:iso/ubuntu-server-22.04.3.iso"
 
-  # Specifying the VM parameters
-  os_type = "ubuntu"  # Ubuntu Server
+#   # Specifying the VM parameters
+#   os_type = "ubuntu"  # Ubuntu Server
 
-  # Define the VM resources
-  cores  = 2
-  sockets = 1
-  memory = 3072 # 3 GB
+#   # Define the VM resources
+#   cores  = 2
+#   sockets = 1
+#   memory = 3072 # 3 GB
 
-  # Disk configuration
-  disk {
-    storage = "local-lvm"  # Replace with your storage pool name
-    size   = "30G"
-    type   = "scsi"
-  }
+#   # Disk configuration
+#   disk {
+#     storage = "local-lvm"  # Replace with your storage pool name
+#     size   = "30G"
+#     type   = "scsi"
+#   }
 
-  # Network interface
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"  # Replace with your bridge name
-    firewall = true
-  }
-}
+#   # Network interface
+#   network {
+#     model  = "virtio"
+#     bridge = "vmbr0"  # Replace with your bridge name
+#     firewall = true
+#   }
+# }
 
-# worker node 1
-resource "proxmox_vm_qemu" "vm_2" {
-  vmid = 202
-  name        = "homelab-k8s-cluster-vm2"
-  target_node = "homelab"
-  iso         = "local:iso/ubuntu-server-22.04.3.iso"
+# # worker node 1
+# resource "proxmox_vm_qemu" "vm_2" {
+#   vmid = 202
+#   name        = "homelab-k8s-cluster-vm2"
+#   target_node = "homelab"
+#   iso         = "local:iso/ubuntu-server-22.04.3.iso"
 
-  # Specifying the VM parameters
-  os_type = "ubuntu"  # Ubuntu Server
+#   # Specifying the VM parameters
+#   os_type = "ubuntu"  # Ubuntu Server
 
-  # Define the VM resources
-  cores  = 2
-  sockets = 1
-  memory = 3072 # 3 GB
+#   # Define the VM resources
+#   cores  = 2
+#   sockets = 1
+#   memory = 3072 # 3 GB
 
-  # Disk configuration
-  disk {
-    storage = "local-lvm"  # Replace with your storage pool name
-    size   = "30G"
-    type   = "scsi"
-  }
+#   # Disk configuration
+#   disk {
+#     storage = "local-lvm"  # Replace with your storage pool name
+#     size   = "30G"
+#     type   = "scsi"
+#   }
 
-  # Network interface
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"  # Replace with your bridge name
-    firewall = true
-  }
-}
-
-# worker node 2
-resource "proxmox_vm_qemu" "vm_3" {
-  vmid = 203
-  name        = "homelab-k8s-cluster-vm3"
-  target_node = "homelab"
-  iso         = "local:iso/ubuntu-server-22.04.3.iso"
-
-  # Specifying the VM parameters
-  os_type = "ubuntu"  # Ubuntu Server
-
-  # Define the VM resources
-  cores  = 2
-  sockets = 1
-  memory = 3072 # 3 GB
-
-  # Disk configuration
-  disk {
-    storage = "local-lvm"  # Replace with your storage pool name
-    size   = "30G"
-    type   = "scsi"
-  }
-
-  # Network interface
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"  # Replace with your bridge name
-    firewall = true
-  }
-}
+#   # Network interface
+#   network {
+#     model  = "virtio"
+#     bridge = "vmbr0"  # Replace with your bridge name
+#     firewall = true
+#   }
+# }
 
 # Output the VM's ID
 output "vm_compute_server" {
   value = proxmox_vm_qemu.compute_server.id
 }
 
-# Output the VM's ID
-output "vm_id1" {
-  value = proxmox_vm_qemu.vm_1.id
-}
+# # Output the VM's ID
+# output "vm_id1" {
+#   value = proxmox_vm_qemu.vm_1.id
+# }
 
-# Output the VM's ID
-output "vm_id2" {
-  value = proxmox_vm_qemu.vm_2.id
-}
+# # Output the VM's ID
+# output "vm_id2" {
+#   value = proxmox_vm_qemu.vm_2.id
+# }
 
-# Output the VM's ID
-output "vm_id3" {
-  value = proxmox_vm_qemu.vm_3.id
-}
