92ad38ba96a8d2803aaf1f0175ed4a80a6cd6aca
Shadman <sahmed85@gatech.edu>
2024-05-13 22:13:20 -0400

    Added fixes to VM deployment to use reserved IPs and static random MAC address. Updated docs and architecture for current layout.

--- homelab_infra/main.tf
+++ homelab_infra/main.tf
@@ -14,24 +14,24 @@ provider "proxmox" {
   pm_password = var.proxmox_password
 }
 
-# vm1 hosting rancher
-resource "proxmox_vm_qemu" "k8s_cluster_vm1" {
+# vm 1
+resource "proxmox_vm_qemu" "nebula_server" {
   vmid        = 201
-  name        = "homelab-k8s-cluster-vm1"
+  name        = "nebula-server"
   target_node = "homelab"
   clone       = "ubuntu-jammy-template" # custom template that I created
   full_clone  = true
-
+  onboot = true
   # Define the VM resources
   # Define the VirtIO SCSI controller
   scsihw    = "virtio-scsi-pci"
   ciuser    = "shada"
-  ipconfig0 = "ip=dhcp"
+  ipconfig0 = "ip=10.0.0.177/24,gw=10.0.0.1"
   qemu_os   = "other"
 
-  cores   = 2
+  cores   = 4
   sockets = 1
-  memory  = 4096 # 4 GB
+  memory  = 7168 # 7 GB
 
   # Disk configuration
   disk {
@@ -44,28 +44,29 @@ resource "proxmox_vm_qemu" "k8s_cluster_vm1" {
   network {
     model    = "virtio"
     bridge   = "vmbr0" # Replace with your bridge name
+    macaddr = "b4:b6:19:2b:2a:52" # random gen MAC Add added for DHCP Reservation
     firewall = true
   }
 }
 
-# vm2 k8s cluster node
-resource "proxmox_vm_qemu" "k8s_cluster_vm2" {
+# vm 2
+resource "proxmox_vm_qemu" "aurora_server" {
   vmid        = 202
-  name        = "homelab-k8s-cluster-vm2"
+  name        = "aurora-server"
   target_node = "homelab"
   clone       = "ubuntu-jammy-template" # custom template that I created
   full_clone  = true
-
+  onboot = true
   # Define the VM resources
   # Define the VirtIO SCSI controller
   scsihw    = "virtio-scsi-pci"
   ciuser    = "shada"
-  ipconfig0 = "ip=dhcp"
+  ipconfig0 = "ip=10.0.0.178/24,gw=10.0.0.1"
   qemu_os   = "other"
 
-  cores   = 2
+  cores   = 4
   sockets = 1
-  memory  = 4096 # 4 GB
+  memory  = 7168 # 7 GB
 
   # Disk configuration
   disk {
@@ -78,56 +79,25 @@ resource "proxmox_vm_qemu" "k8s_cluster_vm2" {
   network {
     model    = "virtio"
     bridge   = "vmbr0" # Replace with your bridge name
+    macaddr = "14:19:10:2b:96:fa" # random gen MAC Add added for DHCP Reservation
     firewall = true
   }
 }
 
-# vm2 k8s cluster node
-resource "proxmox_vm_qemu" "k8s_cluster_vm3" {
-  vmid        = 203
-  name        = "homelab-k8s-cluster-vm3"
-  target_node = "homelab"
-  clone       = "ubuntu-jammy-template" # custom template that I created
-  full_clone  = true
 
-  # Define the VM resources
-  # Define the VirtIO SCSI controller
-  scsihw    = "virtio-scsi-pci"
-  ciuser    = "shada"
-  ipconfig0 = "ip=dhcp"
-  qemu_os   = "other"
 
-  cores   = 2
-  sockets = 1
-  memory  = 4096 # 4 GB
-
-  # Disk configuration
-  disk {
-    storage = "local-lvm" # Replace with your storage pool name
-    size    = "50G"
-    type    = "scsi"
-  }
-
-  # Network interface
-  network {
-    model    = "virtio"
-    bridge   = "vmbr0" # Replace with your bridge name
-    firewall = true
-  }
+# Output the VM's ID
+output "nebula_server_output" {
+  value = proxmox_vm_qemu.nebula_server.id
 }
 
 # Output the VM's ID
-output "k8s_cluster_vm1_output" {
-  value = proxmox_vm_qemu.k8s_cluster_vm1.id
+output "aurora_server_output" {
+  value = proxmox_vm_qemu.aurora_server.id
 }
 
-# Output the VM's ID
-output "k8s_cluster_vm2_output" {
-  value = proxmox_vm_qemu.k8s_cluster_vm2.id
-}
-
-# Output the VM's ID
-output "k8s_cluster_vm3_output" {
-  value = proxmox_vm_qemu.k8s_cluster_vm3.id
-}
+# # Output the VM's ID
+# output "k8s_cluster_vm3_output" {
+#   value = proxmox_vm_qemu.k8s_cluster_vm3.id
+# }
 
